ec2f093d34edf63cd5e3bcf2dcc74e7c366e19f4
MoonerHigh <Maozilox@Gmail.com>
2024-06-05 00:32:32 +0800

    style:删除没用的代码

--- src/main/java/com/moonerhigh/firstai/controller/ChatController.java
+++ src/main/java/com/moonerhigh/firstai/controller/ChatController.java
@@ -12,11 +12,11 @@ import org.springframework.ai.openai.OpenAiChatClient;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.MediaType;
 import org.springframework.http.codec.ServerSentEvent;
-import org.springframework.web.bind.annotation.*;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RestController;
 import reactor.core.publisher.Flux;
-
-import java.lang.invoke.VarHandle;
-import java.util.List;
 
 @Slf4j
 @CrossOrigin
